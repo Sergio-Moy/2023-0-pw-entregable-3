31bfa9ac150e10d34ed54c0e86f4f6c57e1ef53b
unknown <piero.inga.cabrer@gmail.com>
2023-02-11 15:01:11 -0500

    se implemento el registro de un nuevo plato en views

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    restaurant = models.CharField(max_length=30)
--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Dish
+from .serializers import DishSerializer
+from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
+import json
 # Create your views here.
+
+class DishViewSet(viewsets.ModelViewSet):
+    queryset = Dish.objects.all()
+    serializer_class = DishSerializer
+
+    @action(detail=False, methods=['post'])
+    def register(self, request):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(restaurant=request.user.restaurant)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+def obtenerPlatos(request):
+    if request.method == "GET":
+        categoria = request.GET.get("categoria")
+        restaurante = request.GET.get("restaurante")
+
+        if categoria == None or restaurante == None:
+            dictError = {
+                "error": "Debe enviar una categoria y restaurante como query parameters."
+            }
+            return JsonResponse(dictError)
+
+        platos = Dish.objects.filter(category=categoria, restaurant=restaurante)
+
+        dictResponse = {
+            "error": "",
+            "platos": list(platos)
+        }
+        return JsonResponse(dictResponse)
+    else:
+        dictError = {
+            "error": "Tipo de petición no existe"
+        }
+        return JsonResponse(dictError)
