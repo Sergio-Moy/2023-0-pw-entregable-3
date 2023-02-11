71f0770477232a4b25f41803c77e83607bbc983e
unknown <piero.inga.cabrer@gmail.com>
2023-02-11 16:00:46 -0500

    modelo login restaurante creado

--- backend/models.py
+++ backend/models.py
@@ -6,3 +6,11 @@ class Dish(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     restaurant = models.CharField(max_length=30)
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
--- backend/views.py
+++ backend/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rest_framework import viewsets
-from rest_framework.decorators import action
-from rest_framework.response import Response
-from rest_framework import status
+
 from .models import Dish
 
 from django.core import serializers
