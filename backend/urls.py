c5ec02e16f955f5d74ec4e16830efe3ffa204874
unknown <piero.inga.cabrer@gmail.com>
2023-02-11 19:16:18 -0500

    se arreglo modelo integer

--- backend/urls.py
+++ backend/urls.py
@@ -3,7 +3,5 @@ from . import views
 urlpatterns=[
     path("loginrestaurante",views.loginRestaurante),
     path("platos_10/listar", views.obtenerPlatos_10),
-  
-    path("platos_10/listar", views.obtenerPlatos_10),
     path("categorias_10/listar",views.ObtenerCategorias_10)
 ]
